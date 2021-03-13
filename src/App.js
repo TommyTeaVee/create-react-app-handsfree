33a54561b7da87d70d4362b3eb755f921e0cc49b
Oz Ramos <midiblocks@gmail.com>
2021-03-13 12:54:52 -0800

    Adds Handsfree.js (working)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /**
+   * When we visit this route, lets use hands
+   * - You can do this for each route!
+   */
+  window.handsfree.update({
+    hands: true,
+    // weboji: false
+  })
+  window.handsfree.enablePlugins('browser')
+  
+  /**
+   * Click Handlers
+   */
+  function startHandsfree () {window.handsfree.start()}
+  function stopHandsfree () {window.handsfree.stop()}
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {/* These classes are included by handsfree.css */}
+        <p>
+          <button className="handsfree-show-when-stopped handsfree-hide-when-loading" onClick={startHandsfree}>Start handsfree</button>
+          <button className="handsfree-show-when-loading">...loading...</button>
+          <button className="handsfree-show-when-started" onClick={stopHandsfree}>Stop handsfree</button>
+        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Include Handsfree.js and optional styles
+import Handsfree from 'handsfree'
+import 'handsfree/build/lib/assets/handsfree.css'
+
+/**
+ * Setup handsfree.js
+ */
+window.handsfree = new Handsfree({
+  hands: true,
+  showDebug: true,
+  // This is super important. Remember to eject the models into your public path
+  // Windows: xcopy /e node_modules\handsfree\build\lib public
+  // Everywhere else: cp -r node_modules/handsfree/build/lib/* public
+  assetsPath: '/assets'
+})
+
+/**
+ * Render App
+ */
 ReactDOM.render(
   <React.StrictMode>
     <App />
